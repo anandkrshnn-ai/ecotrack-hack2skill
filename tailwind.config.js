1a7aabe56abf72edc917a6e13091ede6094f1a76
Anandakrishnan Damodaran <45256214+anandkrshnn@users.noreply.github.com>
2026-06-21 01:34:12 +0530

    Deploy production compilation of Tailwind CSS to fix layout issues

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': ['Space Grotesk', 'Inter', 'system-ui', 'sans-serif']
+      }
+    }
+  },
+  plugins: [],
+}
